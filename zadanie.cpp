06b10eee78f2fc5e77bd057f52777f6032fb9070
snuczek <26097711+snuk0@users.noreply.github.com>
2021-01-28 12:36:32 +0100

    Add files via upload

---
+++ zadanie.cpp
@@ -0,0 +1,44 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+int maxx = -1, ile = 0;
+
+
+int main()
+{
+    ifstream in("ciagi.txt");
+    int a;
+
+    while (in >> a) 
+    {
+        vector <int> v(a);
+        for(int i=0; i<a; i++){
+            in >> v[i];
+        }
+        bool ok = true;
+        for (int j = 2; j < a; j++)
+        {
+            if (v[j - 1] - v[j - 2] != v[j] - v[j - 1])
+            {
+                ok = false;
+                break;
+            }
+        }
+
+        if (ok)
+        {
+            ile++;
+            maxx = max(maxx, v[1] - v[0]);
+
+        }
+
+    }
+    cout << "Ciagow arytmetycznych: " << ile << "\nNajwieksza roznica: " << maxx;
+
+   
+    
+    return 0;
+}
+
+
